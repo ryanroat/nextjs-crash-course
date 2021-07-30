15c959165f0709d7498cf4d3b9b44092a0b53c8d
Ryan Roat <ryanroat@gmail.com>
2021-07-30 16:13:53 -0400

    nav bar and layout css for all pages

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+
+
 const about = () => {
   return (
     <div>
+      <Head>
+        <title>About WebDev Newz</title>
+      </Head>
       <h1>About</h1>
     </div>
   )
