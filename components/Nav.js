0b2396b4a869a5fc4f416d23b5bac77e045dff6b
Ryan Roat <ryanroat@gmail.com>
2021-07-30 17:00:08 -0400

    added Header to Layout; css tweaks

--- components/Nav.js
+++ components/Nav.js
@@ -11,6 +11,9 @@ const Nav = () => {
         <li>
           <Link href='/about'>About</Link>
         </li>
+        <li>
+          <Link href='/test'>Test</Link>
+        </li>
       </ul>
     </nav>
   )
